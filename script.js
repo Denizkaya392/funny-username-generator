6a19ec35915657f1039593f712879a38f1d67046
Denizkaya392 <denizkaya392@gmail.com>
2025-05-15 05:43:38 +0200

    Update V0.1

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ function generateUsername() {
     return;
   }
 
-  const adjectives = ["crazy", "funny", "silly", "angry", "lazy", "weird"];
-  const suffixes = ["42", "TheBoss", "XD", "X", "TheWaffle", "OnMars"];
+  const adjectives = [
+    "crazy", "funny", "silly", "angry", "lazy", "weird",
+    "sus", "based", "chad", "noob", "pog", "epic",
+    "brainrot", "simp", "cheugy", "yeet", "vibe", "clout",
+    "lit", "thicc", "snatched", "slaps", "ghost", "flex",
+    "lowkey", "salty", "woke", "shady", "stoked", "deadass",
+    "glitchy", "rekt", "savage", "cringe", "dope", "flexin",
+    "drip", "boujee", "snacc", "extra", "suspect", "rekt",
+    "fomo", "glitched", "fire", "thirsty", "spill", "hype",
+    "bussin", "vibin", "swole", "blessed", "clapped"
+  ];
+
+const suffixes = [
+  "42", "TheBoss", "XD", "X", "TheWaffle", "OnMars",
+  "420", "UwU", "Cat", "Z", "LOL", "Fomo",
+  "Nani", "Flex", "Cringe", "Baka", "Sussy",
+  "LOLZ", "BFF", "Troll", "LMAO", "AF", "BRB",
+  "CEO", "NoCap", "Yeet", "LitAF", "GOAT", "Dank",
+  "Beta", "Alpha", "Ghost", "WTF", "Bruh", "Kappa",
+  "UwU", "OwO", "XD", "GG", "AFK", "Poggers",
+  "Zaddy", "Fam", "Vibe", "Skrrt", "Cap", "BigBrain",
+  "Sus", "Thot", "Karen", "FOMO", "Slay", "CloutChaser"
+];
 
   const adj = adjectives[Math.floor(Math.random() * adjectives.length)];
   const suf = suffixes[Math.floor(Math.random() * suffixes.length)];
